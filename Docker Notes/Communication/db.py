1551ec41f4b477abe2a10f25228d5ce6a0ebe2b6
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2019-05-23 00:49:40 +0530

    Update db.py

--- Docker Notes/Communication/db.py
+++ Docker Notes/Communication/db.py
@@ -1,14 +1,10 @@
 import pymysql
-
 # Open database connection
 db = pymysql.connect("mysql-server","root","sagar215","mysql" )
-
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
-
 # Drop table if it already exist using execute() method.
 cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
-
 # Create table as per requirement
 sql = """CREATE TABLE EMPLOYEE (
    FIRST_NAME  CHAR(20) NOT NULL,
@@ -16,7 +12,6 @@ sql = """CREATE TABLE EMPLOYEE (
    AGE INT,
    SEX CHAR(1),
    INCOME FLOAT )"""
-
 cursor.execute(sql)
 FIRST_NAME = "sagar"
 LAST_NAME = "scott"
